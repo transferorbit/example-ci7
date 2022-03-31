1b6947d2b6873fcc4d6abf391587c1e1dc9d81ed
TransferOrbit <transferorbit@icloud.com>
2022-03-31 10:27:35 +0200

    uncommented example.py to generate a fail

--- example.py
+++ example.py
@@ -10,5 +10,5 @@ def subtract(a, b):
     return a + b  # <--- fix this in step 7
 
 # uncomment the following test in step 5
-#def test_subtract():
+def test_subtract():
 #    assert subtract(2, 3) == -1
